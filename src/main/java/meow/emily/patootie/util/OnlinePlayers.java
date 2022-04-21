90857ea84677569e68a4e5cef311e3d3ce54dfbb
Emily Engel <Justsweetluna@gmail.com>
2022-04-21 18:35:17 +0200

    remove useless class

--- src/main/java/meow/emily/patootie/util/OnlinePlayers.java
+++
@@ -1,9 +0,0 @@
-package meow.emily.patootie.util;
-
-import net.minecraft.client.Minecraft;
-
-public class OnlinePlayers {
-    public static String[] getListOfPlayerUsernames() {
-        return Minecraft.getMinecraft().getNetHandler().getPlayerInfoMap().stream().map(object -> object.getGameProfile().getName()).toArray(String[]::new);
-    }
-}
