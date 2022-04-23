4086935f82a08154ebe0be2f3de27f73ef72c94d
Emily Engel <Justsweetluna@gmail.com>
2022-04-23 19:42:51 +0200

    im done. please fucking kill me i dont wanna live anymore. this is way too much fucking shit do to please for the love of god
    Signed-off-by: Emily Engel <Justsweetluna@gmail.com>

--- src/main/java/meow/emily/patootie/events/PlayerEventHandler.java
+++ src/main/java/meow/emily/patootie/events/PlayerEventHandler.java
@@ -3,58 +3,83 @@ package meow.emily.patootie.events;
 import com.mojang.realmsclient.gui.ChatFormatting;
 import meow.emily.patootie.Emily;
 import meow.emily.patootie.util.Utils;
+import net.labymod.main.LabyMod;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.util.ChatComponentText;
 import net.minecraftforge.client.event.RenderPlayerEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent;
 import org.lwjgl.input.Keyboard;
 
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
 public class PlayerEventHandler {
+
     @SubscribeEvent
     public void onPrePlayerRender(RenderPlayerEvent.Pre e) {
-        EntityPlayer enPlayer = e.entityPlayer;
-        if (Emily.getInstance().isRenderPlayers() && !enPlayer.equals(Minecraft.getMinecraft().thePlayer)) {
-            String[] localPlayersToRender = Emily.getInstance().getPlayersToRender().split(",");
-            if (!Utils.isNPC(enPlayer)) {
-                e.setCanceled(false);
-                for (String s : localPlayersToRender) {
-                    if (s.equals(enPlayer.getGameProfile().getName())) {
-                        e.setCanceled(true);
-                      /*  try {
-                                // Just testing smth Emily.getInstance().getVoiceChat().getEnabledSetting().getCurrentValue();
-                                Emily.getInstance().getVoiceChat().getPlayerVolumes().put(UUID.fromString(enPlayer.getGameProfile().getName()), 0);
-                                LabyMod.getInstance().displayMessageInChat("Hidden");
-                        } catch (Exception ex) {
-                          //  System.out.println("Error: " + ex);
+        Emily instance = Emily.getInstance();
+        if (instance.isRenderPlayers()) {
+            EntityPlayer enPlayer = e.entityPlayer;
+            if (instance.isRenderPlayers() && !enPlayer.equals(Minecraft.getMinecraft().thePlayer)) {
+                List<String> localPlayersToRender = instance.getPlayersToRenderString();
+                if (!Utils.isNPC(enPlayer)) {
+                    e.setCanceled(false);
+                    for (String s : localPlayersToRender) {
+                        if (s.equals(enPlayer.getGameProfile().getName())) {
+                            e.setCanceled(true);
+                            if (instance.isVoiceexist()) {
+                                if (instance.isMuted()) {
+                                    mute(enPlayer);
+                                } else {
+                                    unmute(enPlayer);
+                                }
+
+                            }
                         }
-                        try {
-                            Emily.getInstance().getVoiceChat().getPlayerVolumes().get(UUID.fromString(enPlayer.getGameProfile().getName()));
-                        } catch (Exception ex) {
-                           // System.out.println("Error2: " + ex);
-                        }
-                        // Why did no one implement a god damn method to mute Players via the client??????
-                        // this shit here doesnt work afaik */
                     }
                 }
             }
         }
     }
 
+    public void mute(EntityPlayer player) {
+        Emily instance = Emily.getInstance();
+        UUID uuid = player.getUniqueID();
+        instance.getVoiceChat().getPlayerVolumes().put(uuid, 0);
+        instance.savePlayersToRender();
+    }
+
+    public void unmute(EntityPlayer player) {
+        Emily instance = Emily.getInstance();
+        Map<UUID, Integer> playerVolumes = instance.getVoiceChat().getPlayerVolumes();
+        UUID uuid = player.getUniqueID();
+        if (playerVolumes.containsKey(uuid)) {
+            playerVolumes.put(uuid, instance.getVoiceChat().getVolume(uuid));
+        } else {
+            playerVolumes.put(uuid, 100);
+        }
+        instance.savePlayersToRender();
+    }
+
     @SubscribeEvent
     public void onKeyInput(InputEvent.KeyInputEvent e) {
-        if (Emily.getInstance().getKey() > -1) {
-            if (Keyboard.isKeyDown(Emily.getInstance().getKey())) {
-                if (Emily.getInstance().isRenderPlayers()) {
-                    Emily.getInstance().setRenderPlayers(false);
-                    if (Emily.getInstance().isConfigMessage()) {
-                        Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText("[" + ChatFormatting.AQUA + "PH" + ChatFormatting.WHITE + "]" + ChatFormatting.BOLD + ChatFormatting.GREEN + " on"));
+        Emily instance = Emily.getInstance();
+        if (instance.getKey() > -1) {
+            if (Keyboard.isKeyDown(instance.getKey())) {
+                LabyMod labymod = LabyMod.getInstance();
+                if (instance.isRenderPlayers()) {
+                    instance.setRenderPlayers(false);
+                    instance.setMuted(false);
+                    if (instance.isConfigMessage()) {
+                        labymod.displayMessageInChat(ChatFormatting.GRAY + ">>" + "[" + ChatFormatting.AQUA + "PH" + ChatFormatting.WHITE + "]" + ChatFormatting.BOLD + ChatFormatting.GREEN + " on");
                     }
                 } else {
-                    Emily.getInstance().setRenderPlayers(true);
-                    if (Emily.getInstance().isConfigMessage()) {
-                        Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText("[" + ChatFormatting.AQUA + "PH" + ChatFormatting.WHITE + "]" + ChatFormatting.BOLD + ChatFormatting.DARK_RED + " off"));
+                    instance.setRenderPlayers(true);
+                    instance.setMuted(true);
+                    if (instance.isConfigMessage()) {
+                        labymod.displayMessageInChat(ChatFormatting.GRAY + ">>" + "[" + ChatFormatting.AQUA + "PH" + ChatFormatting.WHITE + "]" + ChatFormatting.BOLD + ChatFormatting.DARK_RED + " off");
                     }
                 }
             }
